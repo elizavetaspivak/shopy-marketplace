cc14495788b8ad078b498f4a8cfb30e4db178137
elizavetaspivak <elizabethspivak99@gmail.com>
2024-06-20 20:54:03 +0300

    fix

--- apps/api/src/services/email/email.service.ts
+++ apps/api/src/services/email/email.service.ts
@@ -8,71 +8,89 @@ import logger from 'logger';
 import { EmailServiceConstructorProps, From, SendSendgridTemplateParams, SendTemplateParams } from './email.types';
 
 class EmailService {
-    apiKey: string | undefined;
+  apiKey: string | undefined;
 
-    from: From;
+  from: From;
 
-    constructor({ apiKey, from }: EmailServiceConstructorProps) {
-        this.apiKey = apiKey;
-        this.from = from;
+  constructor({ apiKey, from }: EmailServiceConstructorProps) {
+    this.apiKey = apiKey;
+    this.from = from;
 
-        if (apiKey) sendgrid.setApiKey(apiKey);
+    if (apiKey) sendgrid.setApiKey(apiKey);
+  }
+
+  async sendTemplate<T extends Template>({ to, subject, template, params, attachments }: SendTemplateParams<T>) {
+    if (!this.apiKey) {
+      logger.error('[Sendgrid] API key is not provided');
+      return null;
     }
 
-    async sendTemplate<T extends Template>({ to, subject, template, params, attachments }: SendTemplateParams<T>) {
-        if (!this.apiKey) {
-            logger.error('[Sendgrid] API key is not provided');
-            return null;
-        }
+    const html = await renderEmailHtml({ template, params });
 
-        const html = await renderEmailHtml({ template, params });
-
-        return sendgrid
-            .send({
-                from: this.from,
-                to,
-                subject,
-                html,
-                attachments,
-            })
-            .then(() => {
-                logger.debug(`[Sendgrid] Sent email to ${to}.`);
-                logger.debug({ subject, template, params });
-            });
+    return sendgrid
+      .send({
+        from: this.from,
+        to,
+        subject,
+        html,
+        attachments,
+      })
+      .then(() => {
+        logger.debug(`[Sendgrid] Sent email to ${to}.`);
+        logger.debug({ subject, template, params });
+      });
+  }
+  async sendTemplate1(to: string) {
+    if (!this.apiKey) {
+      logger.error('[Sendgrid] API key is not provided');
+      return null;
     }
 
-    async sendSendgridTemplate({
-                                   to,
-                                   subject,
-                                   templateId,
-                                   dynamicTemplateData,
-                                   attachments,
-                               }: SendSendgridTemplateParams) {
-        if (!this.apiKey) {
-            logger.error('[Sendgrid] API key is not provided');
-            return null;
-        }
+    const msg = {
+      to: to, // Change to your recipient
+      from: this.from, // Change to your verified sender
+      subject: 'Sending with SendGrid is Fun',
+      text: 'and easy to do anywhere, even with Node.js',
+      html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+    };
 
-        return sendgrid
-            .send({
-                from: this.from,
-                to,
-                subject,
-                templateId,
-                dynamicTemplateData,
-                attachments,
-            })
-            .then(() => {
-                logger.debug(`[Sendgrid] Sent email to ${to}.`);
-                logger.debug({ subject, templateId, dynamicTemplateData });
-            });
+    return sendgrid.send(msg).then(() => {
+      logger.debug(`[Sendgrid] Sent email to ${to}.`);
+    });
+  }
+
+  async sendSendgridTemplate({
+    to,
+    subject,
+    templateId,
+    dynamicTemplateData,
+    attachments,
+  }: SendSendgridTemplateParams) {
+    if (!this.apiKey) {
+      logger.error('[Sendgrid] API key is not provided');
+      return null;
     }
+
+    return sendgrid
+      .send({
+        from: this.from,
+        to,
+        subject,
+        templateId,
+        dynamicTemplateData,
+        attachments,
+      })
+      .then(() => {
+        logger.debug(`[Sendgrid] Sent email to ${to}.`);
+        logger.debug({ subject, templateId, dynamicTemplateData });
+      });
+  }
 }
 
 export default new EmailService({
-    apiKey: config.SENDGRID_API_KEY,
-    from: {
-        email: 'notifications@ship.com',
-        name: 'Ship',
-    },
+  apiKey: config.SENDGRID_API_KEY,
+  from: {
+    email: 'itsmehelloxo@gmail.com',
+    name: 'Shopy',
+  },
 });
