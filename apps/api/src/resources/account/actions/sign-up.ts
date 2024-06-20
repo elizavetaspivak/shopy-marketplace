f172bf77c6b8100689e33f7d42a5a2ce4ac6610c
elizavetaspivak <elizabethspivak99@gmail.com>
2024-06-20 16:31:25 +0300

    fix

--- apps/api/src/resources/account/actions/sign-up.ts
+++ apps/api/src/resources/account/actions/sign-up.ts
@@ -47,6 +47,16 @@ async function handler(ctx: AppKoaContext<ValidatedData>) {
     signupToken,
   });
 
+  await emailService.sendTemplate<Template.VERIFY_EMAIL>({
+    to: user.email,
+    subject: 'Please Confirm Your Email Address for Ship',
+    template: Template.VERIFY_EMAIL,
+    params: {
+      firstName: '',
+      href: `${config.API_URL}/account/verify-email?token=${signupToken}`,
+    },
+  });
+
   if (config.IS_DEV) {
     ctx.body = { signupToken };
     return;
